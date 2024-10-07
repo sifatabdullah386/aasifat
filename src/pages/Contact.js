cb0bdca1c9094cb938f82a91b23bd461fb329619
sifatabdullah386 <sifatabdullah386@gmail.com>
2024-10-07 22:58:32 +0600

    Intial

---
+++ src/pages/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+import EmailLink from '../components/Contact/EmailLink';
+import ContactIcons from '../components/Contact/ContactIcons';
+
+const Contact = () => (
+  <Main
+    title="Contact"
+    description="Contact Md. Abdullah Al Sifat via email @ sifatabdullah386@google.com"
+  >
+    <article className="post" id="contact">
+      <header>
+        <div className="title">
+          <h2>
+            <Link to="/contact">Contact</Link>
+          </h2>
+        </div>
+      </header>
+      <div className="email-at">
+        <p>Feel free to get in touch. You can email me at: </p>
+        <EmailLink />
+      </div>
+      <ContactIcons />
+    </article>
+  </Main>
+);
+
+export default Contact;
